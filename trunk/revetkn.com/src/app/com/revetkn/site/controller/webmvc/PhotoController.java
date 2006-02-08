396040d967d489fe2d62b53aac0f14c942fd5407
revetkn <revetkn@02ad7f3d-100a-0410-a8d9-b32fdd19b42a>
2006-02-08 04:55:54 +0000

    initial commit of the photo page controller

---
+++ trunk/revetkn.com/src/app/com/revetkn/site/controller/webmvc/PhotoController.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2006 Mark Allen [mark.a.allen@gmail.com]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package com.revetkn.site.controller.webmvc;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.revetkn.site.model.domain.WebImage;
+import com.revetkn.site.model.service.WebImageService;
+
+/**
+ * Web controller that finds photo feed data and places it in the request.
+ * @author <a href="mailto:mark.a.allen@gmail.com">Mark Allen</a>
+ * @version $Id$
+ * @since 0.1
+ */
+public class PhotoController implements Controller
+{
+    /**
+     * @see org.springframework.web.servlet.mvc.Controller#handleRequest(
+     * javax.servlet.http.HttpServletRequest,
+     * javax.servlet.http.HttpServletResponse)
+     */
+    public ModelAndView handleRequest(HttpServletRequest request,
+            HttpServletResponse response) throws Exception
+    {
+        List<WebImage> photos = webImageService
+                .findImages(feedUrl);
+
+        return new ModelAndView(".photo-display", "photos", photos);
+    }
+
+    /**
+     * Sets the webImageService.
+     * @param webImageService The webImageService to set.
+     */
+    public void setWebImageService(WebImageService webImageService)
+    {
+        this.webImageService = webImageService;
+    }
+
+    /**
+     * Sets the feedUrl.
+     * @param feedUrl The feedUrl to set.
+     */
+    public void setFeedUrl(String feedUrl)
+    {
+        this.feedUrl = feedUrl;
+    }
+
+    /**
+     * Used to retrieve images from the web.
+     */
+    private WebImageService webImageService;
+
+    /**
+     * The URL of the feed to retrieve.
+     */
+    private String feedUrl;
+}
